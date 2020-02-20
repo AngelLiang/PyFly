dbfd1e754f48e965a5cc3a0760584046bce34495
AngelLiang <pl01665077@163.com>
2020-02-20 17:03:35 +0800

    Added Pipfile*

--- fly_bbs/config.py
+++ fly_bbs/config.py
@@ -1,13 +1,15 @@
 import os
 from flask_uploads import IMAGES
+
+
 class Dev:
     MONGO_URI = "mongodb://127.0.0.1:27017/pyfly"
-    MAIL_SERVER = 'smtp.163.com'
+    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.qq.com')
     MAIL_PROT = 465
     MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-    MAIL_USERNAME = 'xiuqianli_2015@163.com'
-    MAIL_PASSWORD = ''
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEBUG = True
     MAIL_SUBJECT_PREFIX = '[PyFly]-'
     WTF_CSRF_ENABLED = False
@@ -16,7 +18,8 @@ class Dev:
     WHOOSH_PATH = os.path.join(os.getcwd(), 'whoosh_indexes')
 
     USE_CACHE = True
-    CACHE_TYPE = 'redis'
+    # CACHE_TYPE = 'redis'
+    CACHE_TYPE = 'simple'
     CACHE_REDIS_HOST = '172.16.1.202'
     CACHE_REDIS_PORT = 6379
     CACHE_REDIS_PASSWORD = '456123'
@@ -25,6 +28,7 @@ class Dev:
 
 class Pud:
     pass
+
 
 config = {
     "Dev": Dev,
---
+++ note.md
@@ -0,0 +1,12 @@
+# 说明文档
+
+## 快速开始
+
+    pipenv install
+    python manager.py runserver
+
+然后访问 http://127.0.0.1:5000/
+
+- 管理员帐号： admin
+- 管理员密码： amdin
+
